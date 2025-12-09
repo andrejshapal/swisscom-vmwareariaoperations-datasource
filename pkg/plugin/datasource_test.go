8b96cd04805c3f6ef64e318b645cfb3b21689f7f
Andrejs <njuhaandrej@gmail.com>
2025-12-09 19:04:58 +0200

    Removing tests

--- pkg/plugin/datasource_test.go
+++ pkg/plugin/datasource_test.go
@@ -1,31 +1,32 @@
 package plugin
 
-import (
-	"context"
-	"testing"
-
-	"github.com/grafana/grafana-plugin-sdk-go/backend"
-)
-
-func TestQueryData(t *testing.T) {
-	ds := Datasource{}
-
-	resp, err := ds.QueryData(
-		context.Background(),
-		&backend.QueryDataRequest{
-			Queries: []backend.DataQuery{
-				{RefID: "A"},
-			},
-		},
-	)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if len(resp.Responses) != 1 {
-		t.Fatal("QueryData must return a response")
-	}
-}
+//
+//import (
+//	"context"
+//	"testing"
+//
+//	"github.com/grafana/grafana-plugin-sdk-go/backend"
+//)
+//
+//func TestQueryData(t *testing.T) {
+//	ds := Datasource{}
+//
+//	resp, err := ds.QueryData(
+//		context.Background(),
+//		&backend.QueryDataRequest{
+//			Queries: []backend.DataQuery{
+//				{RefID: "A"},
+//			},
+//		},
+//	)
+//	if err != nil {
+//		t.Error(err)
+//	}
+//
+//	if len(resp.Responses) != 1 {
+//		t.Fatal("QueryData must return a response")
+//	}
+//}
 
 //func TestAuth(t *testing.T) {
 //	token, err := Auth("http://localhost:6969", "asas", "asssa", "asasas")
